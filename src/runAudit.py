a56f6f5d0df62f5ad693c929732a8a5d73ed60a8
AJ Dehn <ajdehn76@gmail.com>
2026-04-05 18:11:31 -0500

    Adding session to audit class.

--- src/runAudit.py
+++ src/runAudit.py
@@ -1,6 +1,5 @@
-from dotenv import load_dotenv
 import controlTesting
-from utils import confirm_delete_folder, save_json, load_config
+from utils import confirm_delete_folder, save_json, load_config, create_session, get_aws_account_id
 from buildReport import generate_pdf_report
 from evidenceClient import EvidenceClient
 
@@ -13,57 +12,53 @@ class Audit:
         self.evidence_folder = evidence_folder                      # Name of the evidence_folder
         self.config = load_config(config_file_path)                 # Control and sample exclusions
         self.evidence_client = EvidenceClient(base_path=evidence_folder, debug=False)
-        self.in_scope_regions = controlTesting.get_regions(self)
-        self.aws_account_id = controlTesting.get_aws_account_id()
+        self.session = create_session()
+        self.in_scope_regions = controlTesting.get_regions(self)        
+        self.aws_account_id = get_aws_account_id(self)
 
 if __name__ == "__main__":
-    # Load variables from .env file
-    load_dotenv()
-    audit = Audit()
-
     print("\nRunning the AWS Audit Playbook (maintained by AJ Dehn - AuditOps.io)\n")
     controls = []
-
+    audit = Audit()
     confirm_delete_folder(audit.evidence_folder)
 
-    controls.append(controlTesting.test_root_mfa_enabled(audit, "IAM Root MFA"))
-    controls.append(controlTesting.test_root_no_access_keys(audit, "IAM Root Access Key"))
-    controls.append(controlTesting.test_iam_users_mfa(audit, "IAM User MFA"))
-    controls.append(controlTesting.test_iam_access_key_age(audit, "IAM User Key Age"))
-    controls.append(controlTesting.test_iam_password_policy(audit, "IAM Password"))
-    # TODO: Add IAM tests (IAM User Stale Access Keys)
+    control_definitions = [
+        ("IAM Root MFA", controlTesting.test_root_mfa_enabled),
+        ("IAM Root Access Key", controlTesting.test_root_no_access_keys),
+        ("IAM User MFA", controlTesting.test_iam_users_mfa),
+        ("IAM User Key Age", controlTesting.test_iam_access_key_age),
+        ("IAM Password", controlTesting.test_iam_password_policy),
+        ("S3 Encryption", controlTesting.test_s3_encryption),
+        ("S3 Public Access", controlTesting.test_s3_public_access),
+        ("S3 Tags", controlTesting.test_s3_tags),
+        ("RDS Backup Retention", controlTesting.test_rds_backup_retention),
+        ("RDS Encryption", controlTesting.test_rds_encryption),
+        ("RDS Public Access", controlTesting.test_rds_public_access),
+        ("RDS Automatic Upgrades", controlTesting.test_rds_auto_minor_version_upgrade),
+        ("RDS Deletion Protection", controlTesting.test_rds_deletion_protection),
+        ("RDS Tags", controlTesting.test_rds_tags),
+        ("EBS Volume Encryption", controlTesting.test_ebs_volume_encryption),
+        ("EBS Encryption Default", controlTesting.test_ebs_default_encryption),
+        ("EBS Tags", controlTesting.test_ebs_tags),
+        ("EC2 Tags", controlTesting.test_ec2_tags),
+        ("EC2 Security Group Tags", controlTesting.test_ec2_security_group_tags),
+        ("Lambda Tags", controlTesting.test_lambda_tags),
+        ("CloudTrail Multi-Region", controlTesting.test_cloudtrail_global_logging),
+        ("CloudTrail Log File Validation", controlTesting.test_cloudtrail_log_file_validation),
+        ("CloudTrail S3 Bucket Protection", controlTesting.test_cloudtrail_s3_bucket_protection),
+        ("CloudTrail Logging Recent Stops", controlTesting.test_cloudtrail_logging_recent_stops),
+        ("Web Application Firewall Enabled", controlTesting.test_waf_enabled),
+    ]
 
-    controls.append(controlTesting.test_s3_encryption(audit, "S3 Encryption"))
-    controls.append(controlTesting.test_s3_public_access(audit, "S3 Public Access"))
-    controls.append(controlTesting.test_s3_tags(audit, "S3 Tags"))
+    controls = []
+    for control_id, control_fn in control_definitions:
+        controls.append(controlTesting.run_control_safely(audit, control_fn, control_id))
+
+    # TODO: Add IAM tests (IAM User Stale Access Keys)    
     # TODO: Add S3 secure transport test
-    # TODO: Add S3 object owner check
-
-    controls.append(controlTesting.test_rds_backup_retention(audit, "RDS Backup Retention"))
-    controls.append(controlTesting.test_rds_encryption(audit, "RDS Encryption"))
-    controls.append(controlTesting.test_rds_public_access(audit, "RDS Public Access"))
-    controls.append(controlTesting.test_rds_auto_minor_version_upgrade(audit, "RDS Automatic Upgrades"))
-    controls.append(controlTesting.test_rds_deletion_protection(audit, "RDS Deletion Protection"))
-    controls.append(controlTesting.test_rds_tags(audit, "RDS Tags"))
-
-    controls.append(controlTesting.test_ebs_volume_encryption(audit, "EBS Volume Encryption"))
-    controls.append(controlTesting.test_ebs_default_encryption(audit, "EBS Encryption Default"))
-    controls.append(controlTesting.test_ebs_tags(audit, "EBS Tags"))
-    controls.append(controlTesting.test_ec2_tags(audit, "EC2 Tags"))
-    controls.append(controlTesting.test_ec2_security_group_tags(audit, "EC2 Security Group Tags"))
-    # TODO: Add EC2 Public Ports (22, RDS, all ports, etc)
-
-    controls.append(controlTesting.test_lambda_tags(audit, "Lambda Tags"))
-
-    controls.append(controlTesting.test_cloudtrail_global_logging(audit, "CloudTrail Multi-Region"))
-    controls.append(controlTesting.test_cloudtrail_log_file_validation(audit, "CloudTrail Log File Validation"))
-    controls.append(controlTesting.test_cloudtrail_s3_bucket_protection(audit, "CloudTrail S3 Bucket Protection"))
-    controls.append(controlTesting.test_cloudtrail_logging_recent_stops(audit, "CloudTrail Logging Recent Stops"))
-
-    controls.append(controlTesting.test_waf_enabled(audit, "Web Application Firewall Enabled"))
+    # TODO: Add S3 object owner check    
+    # TODO: Add EC2 Public Ports (22, RDS, all ports, etc)    
     # TODO: Add WAF Tags
-
-    # TODO: Add AWS Client Cache
     # TODO: Add GuardDuty Enabled for regions with resources.
     # TODO: Add GuardDuty findings resolved within a set time period.
     # TODO: Add GuardDuty findings sent to EventBridge every 15 minutes (default is 6 hours).
