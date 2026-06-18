a013b1cb4863a514ae6e7ad9834881d1bfdb1e82
brosenlieb <11198948+brosenlieb@users.noreply.github.com>
2026-06-18 11:14:06 -0700

    Fixing issue with default config file.

--- src/sample.py
+++ src/sample.py
@@ -30,28 +30,29 @@ class Sample:
         # Returns true if when sample is excluded in the config file.
         sample_key = tuple(sorted(self.sample_id.items()))
 
-        exclusion = audit.sample_exclusion_index.get((test.test_id, sample_key))
-
-        if not exclusion:
-            return False
-
-        is_excluded = False
-        # Permanent exclusions
-        if exclusion["permanent"]:
-            is_excluded = True
-
-        # Check if exclusion is current
-        exp = exclusion["expiration_date"]
-        if exp and date.fromisoformat(exp) >= date.today():
-            is_excluded = True
-
-        # Document result
-        if is_excluded:
-            self.is_excluded = True
-            self.comments = "Sample is excluded. See config.json"
-            # Add excluded sample to tests.
-            test.samples.append(self)
-            return True
+        if audit.sample_exclusion_index:
+            is_excluded = False
+            exclusion = audit.sample_exclusion_index.get((test.test_id, sample_key))
+            if exclusion:    
+                # Permanent exclusions
+                if exclusion["permanent"]:
+                    is_excluded = True
+                else:
+                    # Check if exclusion is current
+                    exp = exclusion["expiration_date"]
+                    if exp and date.fromisoformat(exp) >= date.today():
+                        is_excluded = True
+                    else:
+                        is_excluded = False
+            # Document result
+            if is_excluded:
+                self.is_excluded = True
+                self.comments = "Sample is excluded. See config.json"
+                # Add excluded sample to tests.
+                test.samples.append(self)
+                return True
+            else:
+                # Exclusion was expired or invalid date format.
+                return False     
         else:
-            # Exclusion was expired or invalid date format.
             return False
